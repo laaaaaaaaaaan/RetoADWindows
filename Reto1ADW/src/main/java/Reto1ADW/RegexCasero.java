d7386cfc3f85cc322fc4933df2749a2be60c5543
IN1DM3B_06 <IN1DM3B_06@H015112.NET>
2019-12-04 14:19:36 +0100

    Creada la clase de regexcasero

---
+++ Reto1ADW/src/main/java/Reto1ADW/RegexCasero.java
@@ -0,0 +1,17 @@
+package Reto1ADW;
+
+import java.util.Scanner;
+
+public class RegexCasero {
+
+	public void validarPorExpresion() {
+		String texto ="";
+		String expres = "";
+		Scanner reader = new Scanner(System.in);
+		//Mostrar las experesiones regulares que tenemos.
+		System.out.println("Introduzca texto ");
+		texto = reader.next();
+		System.out.println("Introduzca la expresion regular");
+		expres = reader.next();
+	}
+}
