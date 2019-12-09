c7a7ec33252c81b0ab5af2c8b281db069bc35d32
IN1DM3B_06 <IN1DM3B_06@H015112.NET>
2019-12-09 12:58:00 +0100

    paso a limpio

--- Reto1ADW/src/main/java/Reto1ADW/Launcher.java
+++ Reto1ADW/src/main/java/Reto1ADW/Launcher.java
@@ -1,5 +1,6 @@
 package Reto1ADW;
 
+import java.io.File;
 import java.io.IOException;
 import java.util.Scanner;
 
@@ -12,15 +13,14 @@ public class Launcher {
 		PasarHexadecimal pH = new PasarHexadecimal();
 		Octal octal = new Octal();
 		Separador sep = new Separador();
-		String[] lineasEnviadas = {"alosha,Olefir,18,Lander Haz Algo","Lander,Arellano,23,Alosha Haz Algo"};
-	/*	String archivoCsv = "ficheros"+ File.separLectorDeTxt(archivoTxt);
-		lecturaXml.funcionDeLator+"ArchivoCSV.csv";
+		String[] lineasEnviadas = {"alosha,Olefir,18,Ejemplo 1","Lander,Arellano,23,Ejemplo 2"};
+		String archivoCsv = "ficheros"+ File.separator+"ArchivoCSV.csv";
 		String archivoTxt =  "ficheros"+ File.separator+"ArchivoTxt.txt";
 		String archivoXml =  "ficheros"+ File.separator+"datos.xml";
 		LectorDatosPorTeclado.LectorDeAlfabetico();
 		lecturaCSV.funcionesDeLectorCsv(archivoCsv);
-		lecturaTxt.funcionDeectorDeXML(archivoXml);
-		EscritorTxt.funcionDeEscrituraDeXML(archivoXml);*/
+		lecturaXml.funcionDeLectorDeXML(archivoXml);
+		EscritorTxt.funcionDeEscrituraDeXML(archivoXml);
 		sep.separador(lineasEnviadas,",");
 		System.out.println("Introduce el numero decimal que quieres convertir a hexadecimal");
 		num=reader.nextInt();
